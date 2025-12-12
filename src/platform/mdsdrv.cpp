a07323e6155aef18ea50c63027c79020b27fb31b
garrettjwilke <garrett@system76.com>
2025-12-11 17:30:23 -0700

    comm numbers must be 0-63

--- src/platform/mdsdrv.cpp
+++ src/platform/mdsdrv.cpp
@@ -852,13 +852,27 @@ void MDSDRV_Track_Writer::parse_platform_event(const Tag& tag)
 			error("pcmmode argument must be between 2 and 3");
 		converted_events.push_back(MDSDRV_Event(MDSDRV_Event::PCMMODE, data));
 	}
-	else if(iequal(tag[0], "comm")) // Communication byte
+	else if (iequal(tag[0], "comm")) // Communication byte
 	{
-		if(tag.size() < 2)
+		if (tag.size() < 2)
 			error("not enough parameters for 'comm' command");
-		uint8_t data = std::strtol(tag[1].c_str(), 0, 0);
+
+		char* endptr;
+		long value = std::strtol(tag[1].c_str(), &endptr, 0);
+
+		// Check for parse errors
+		if (*endptr != '\0')
+			error("invalid number for 'comm' command: " + tag[1]);
+
+		// Check range
+		if (value < 0 || value > 63)
+			error("'comm' value out of range (must be 0–63): " + tag[1]);
+
+		uint8_t data = static_cast<uint8_t>(value);
+
 		converted_events.push_back(MDSDRV_Event(MDSDRV_Event::COMM, data));
 	}
+
 	else if(iequal(tag[0], "cmd")) // Direct command
 	{
 		if(tag.size() < 2)
